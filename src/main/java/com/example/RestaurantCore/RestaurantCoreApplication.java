e02deac1ebdb82258d52b297cf7394e7170a31c3
Karthik Reddy <karthikreddy573y@gmail.com>
2023-12-06 23:49:07 +0530

    Initial commit

---
+++ src/main/java/com/example/RestaurantCore/RestaurantCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestaurantCore/RestaurantCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
